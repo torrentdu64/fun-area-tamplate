30b78fe64c41d15e45c720b475ece14fa7b39f30
torrent benoit <torrentdu64@gmail.com>
2016-04-03 15:59:04 +0200

    hide to config

--- config.rb
+++ config.rb
@@ -90,7 +90,8 @@ activate :deploy do |deploy|
 
   # Optional Settings
   # deploy.remote = 'custom-remote' # remote name or git url, default: origin
-   deploy.branch = 'custom-branch'  default: gh-pages
+  #deploy.branch = 'custom-branch'
+  # default: gh-pages
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
